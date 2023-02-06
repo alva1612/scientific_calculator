898b9015dc00ac2ed762ea8e296147a271527128
Alvaro <alvaro.guillenc1612@gmail.com>
2023-02-05 23:48:32 -0500

    added err handling like docs and loop asking

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use regex::{Regex};
+use regex::Regex;
 
 fn main() {
-
     //REGEX
     let re_add = Regex::new(r"(\d+)\s?\+\s?(\d+)").unwrap();
     let re_mult = Regex::new(r"(\d+)\s?\*\s?(\d+)").unwrap();
@@ -9,91 +8,109 @@ fn main() {
     let re_diff = Regex::new(r"(\d+)\s?\-\s?(\d+)").unwrap();
 
     //DATA DEL USUARIO
-    println!("Ingrese la operación");
-    let mut expression = String::new();
 
-    std::io::stdin().read_line(&mut expression).unwrap();
-
-    //DIVSION
     loop {
-        //OPERACIONES
-        let caps = re_div.captures(expression.as_str());
+        let mut expression = String::new();
+        let mut exists_error = false;
+        println!("Ingrese la operación");
+        std::io::stdin()
+            .read_line(&mut expression)
+            .expect("Couldn't read input");
+        //DIVSION
+        loop {
+            //OPERACIONES
+            let caps = re_div.captures(expression.as_str());
 
-        if caps.is_none() {
-            break;
+            if caps.is_none() {
+                break;
+            }
+
+            let caps = caps.unwrap();
+
+            let cap_expression = caps.get(0).unwrap().as_str();
+            let left_value: i32 = match caps.get(1).unwrap().as_str().parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    exists_error = true;
+                    println!("Input error");
+                    break;
+                }
+            };
+            let rigth_value: i32 = match caps.get(2).unwrap().as_str().parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    exists_error = true;
+                    println!("Input error");
+                    break;
+                }
+            };
+
+            let result = left_value / rigth_value;
+
+            expression = expression.replace(cap_expression, &result.to_string());
+        }
+        //MULTIPLICATION
+        loop {
+            //OPERACIONES
+            let caps = re_mult.captures(expression.as_str());
+
+            if caps.is_none() {
+                break;
+            }
+
+            let caps = caps.unwrap();
+
+            let cap_expression = caps.get(0).unwrap().as_str();
+            let left_value: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
+            let rigth_value: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
+
+            let result = left_value * rigth_value;
+
+            expression = expression.replace(cap_expression, &result.to_string());
+        }
+        //ADDITION
+        loop {
+            //OPERACIONES
+            let caps = re_add.captures(expression.as_str());
+
+            if caps.is_none() {
+                break;
+            }
+
+            let caps = caps.unwrap();
+
+            let cap_expression = caps.get(0).unwrap().as_str();
+            let left_value: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
+            let rigth_value: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
+
+            let addition = left_value + rigth_value;
+
+            expression = expression.replace(cap_expression, &addition.to_string());
+        }
+        //DIFFERENCE
+        loop {
+            //OPERACIONES
+            let caps = re_diff.captures(expression.as_str());
+
+            if caps.is_none() {
+                break;
+            }
+
+            let caps = caps.unwrap();
+
+            let cap_expression = caps.get(0).unwrap().as_str();
+            let left_value: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
+            let rigth_value: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
+
+            let addition = left_value - rigth_value;
+
+            expression = expression.replace(cap_expression, &addition.to_string());
+        }
+        //MOSTAR RESULTADO
+        if exists_error {
+            continue;
         }
 
-        let caps = caps.unwrap();
-
-        let cap_expression = caps.get(0).unwrap().as_str();
-        let left_value: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
-        let rigth_value: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
-
-
-        let result = left_value / rigth_value;
-
-        expression = expression.replace(cap_expression, &result.to_string());
+        println!("Res: {}", expression);
     }
-    //MULTIPLICATION
-    loop {
-        //OPERACIONES
-        let caps = re_mult.captures(expression.as_str());
-
-        if caps.is_none() {
-            break;
-        }
-
-        let caps = caps.unwrap();
-
-        let cap_expression = caps.get(0).unwrap().as_str();
-        let left_value: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
-        let rigth_value: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
-
-
-        let result = left_value * rigth_value;
-
-        expression = expression.replace(cap_expression, &result.to_string());
-    }
-    //ADDITION
-    loop {
-        //OPERACIONES
-        let caps = re_add.captures(expression.as_str());
-
-        if caps.is_none() {
-            break;
-        }
-
-        let caps = caps.unwrap();
-
-        let cap_expression = caps.get(0).unwrap().as_str();
-        let left_value: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
-        let rigth_value: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
-
-
-        let addition = left_value + rigth_value;
-
-        expression = expression.replace(cap_expression, &addition.to_string());
-    }
-    //DIFFERENCE
-    loop {
-        //OPERACIONES
-        let caps = re_diff.captures(expression.as_str());
-
-        if caps.is_none() {
-            break;
-        }
-
-        let caps = caps.unwrap();
-
-        let cap_expression = caps.get(0).unwrap().as_str();
-        let left_value: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
-        let rigth_value: i32 = caps.get(2).unwrap().as_str().parse().unwrap();
-
-
-        let addition = left_value - rigth_value;
-
-        expression = expression.replace(cap_expression, &addition.to_string());
-    }
-    //MOSTAR RESULTADO
-    println!("Res: {}", expression);
 }
